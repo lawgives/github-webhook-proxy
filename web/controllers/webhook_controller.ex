ff29935b2f04948d204c953a850c65cd680dddb4
Ho-Sheng Hsiao <hosh@legal.io>
2016-11-19 19:16:32 +0000

    [API] Handle errors when proxying

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -6,7 +6,23 @@ defmodule WebhookProxy.WebhookController do
 
   def webhook(conn, %{"repository_url" => repository_url}) do
     # Proxy with the real credentials
-    HTTPoison.post "http://username:password@localhost/basic_auth", "repository_url="<>repository_url, %{}
+    case HTTPoison.post "http://username:password@localhost/basic_auth", "repository_url="<>repository_url, %{} do
+      {:ok, %HTTPoison.Response{status_code: 200, body: _}} ->
+        conn
+        |> send_resp(200, "OK")
+      {:ok, %HTTPoison.Response{status_code: 201, body: _}} ->
+        conn
+        |> send_resp(200, "OK")
+      {:ok, %HTTPoison.Response{status_code: code, body: body}} ->
+        IO.puts ["Error when proxying. code=", code, " body=", body]
+        conn
+        |> send_resp(500, "Error")
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.puts "Error when proxying. reason:"
+        IO.inspect(reason)
+        conn
+        |> send_resp(500, "Error")
+    end
 
     conn
     |> send_resp(200, "OK")
