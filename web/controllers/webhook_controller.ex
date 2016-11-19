0dc7918826c0ba5e2459deaee7a9b0213068d2cf
Ho-Sheng Hsiao <hosh@legal.io>
2016-11-19 19:05:01 +0000

    [API] Post to webhook if request is authorized and well-formed

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -5,6 +5,9 @@ defmodule WebhookProxy.WebhookController do
   plug BasicAuth, use_config: { :basic_auth, :webhook }
 
   def webhook(conn, %{"repository_url" => repository_url}) do
+    # Proxy with the real credentials
+    HTTPoison.post "http://username:password@localhost/basic_auth", "repository_url="<>repository_url, %{}
+
     conn
     |> send_resp(200, "OK")
   end
